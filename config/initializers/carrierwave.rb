3d9259b982bfcc9138aea1650c400a32f1745a30
ywatanabe315 <nabeo.dendai.master31@gmail.com>
2020-10-17 13:58:13 +0900

    fix carrierwave url

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,3 +1,3 @@
 CarrierWave.configure do |config|
-    config.asset_host = 'http://localhost:3001'
+    config.asset_host = Rails.env.production? 'https://image-uploader-front-nabeo.herokuapp.com/' : 'http://localhost:3001'
   end
